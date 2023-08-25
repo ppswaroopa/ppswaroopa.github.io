9a74b53146592e93832866f666ae26e79b61ff07
Pranava Swaroopa <pranavaswaroopaa2000@gmail.com>
2023-08-25 00:09:08 -0400

    grad_update

--- src/script/script.js
+++ src/script/script.js
@@ -1,51 +1,73 @@
+function goup() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
 function on(a) {
-  if (a == 1) {
-    document.getElementById("sev-wea-det").style.display = "flex";
-  }
-  if (a == 2) {
-    document.getElementById("autobot-det").style.display = "flex";
-  }
-  if (a == 3) {
-    document.getElementById("astar-det").style.display = "flex";
-  }
-  if (a == 4) {
-    document.getElementById("agrawal-lab-det").style.display = "flex";
-  }
-  if (a == 5) {
-    document.getElementById("off-road-det").style.display = "flex";
-  }
-  if (a == 6) {
-    document.getElementById("sysmon-det").style.display = "flex";
-  }
-  if (a == 7) {
-    document.getElementById("snakegame-det").style.display = "flex";
+  switch (a) {
+    case 1:
+      document.getElementById("sev-wea-det").style.display = "flex";
+      break;
+    case 2:
+      document.getElementById("autobot-det").style.display = "flex";
+      break;
+    case 3:
+      document.getElementById("astar-det").style.display = "flex";
+      break;
+    case 4:
+      document.getElementById("agrawal-lab-det").style.display = "flex";
+      break;
+    case 5:
+      document.getElementById("off-road-det").style.display = "flex";
+      break;
+    case 6:
+      document.getElementById("sysmon-det").style.display = "flex";
+      break;
+    case 7:
+      document.getElementById("snakegame-det").style.display = "flex";
+      break;
+    default:
+      break;
   }
   document.querySelector('body').style.overflow = "hidden";
+  if (screen.availWidth>481) {
+    document.getElementById('navbar').style.display = "none";
+  }
 }
 
 function off(b) {
-  if (b == 1) {
-    document.getElementById("sev-wea-det").style.display = "none";
-  }
-  if (b == 2) {
-    document.getElementById("autobot-det").style.display = "none";
-  }
-  if (b == 3) {
-    document.getElementById("astar-det").style.display = "none";
-  }
-  if (b == 4) {
-    document.getElementById("agrawal-lab-det").style.display = "none";
-  }
-  if (b == 5) {
-    document.getElementById("off-road-det").style.display = "none";
-  }
-  if (b == 6) {
-    document.getElementById("sysmon-det").style.display = "none";
-  }
-  if (b == 7) {
-    document.getElementById("snakegame-det").style.display = "none";
+  switch (b) {
+    case 1:
+      document.getElementById("sev-wea-det").style.display = "none";
+      break;
+    case 2:
+      document.getElementById("autobot-det").style.display = "none";
+      break;
+    case 3:
+      document.getElementById("astar-det").style.display = "none";
+      break;
+    case 4:
+      document.getElementById("agrawal-lab-det").style.display = "none";
+      break;
+    case 5:
+      document.getElementById("off-road-det").style.display = "none";
+      break;
+    case 6:
+      document.getElementById("sysmon-det").style.display = "none";
+      break;
+    case 7:
+      document.getElementById("snakegame-det").style.display = "none";
+      break;
+    default:
+      break;
   }
   document.querySelector('body').style.overflow = "auto";
+  if (screen.availWidth>481) {
+    document.getElementById('navbar').style.display = "block";
+  }
+  // document.getElementById('navbar').style.display = "block";
 }
 
 const navLinks = document.querySelectorAll('.navbar a');
@@ -55,28 +77,43 @@ navLinks.forEach(link => {
         event.preventDefault(); // Prevent default link behavior
         const targetSectionId = link.getAttribute('href');
         const targetSection = document.querySelector(targetSectionId);
-        
         if (targetSection) {
+            // navbar.classList.toggle("active");
             // Calculate the scroll position of the target section
-            const targetOffset = targetSection.offsetTop;
-            
+            targetOffset = targetSection.offsetTop;
             // Scroll to the target section with smooth behavior
             window.scrollTo({
                 top: targetOffset,
                 behavior: 'smooth'
             });
+
         }
     });
 });
 
-const menuToggle = document.querySelector('.menu-toggle');
-const navbar = document.getElementsByClassName('navbar');
-// const burger = document.querySelector('.menu-toggle i');
+document.addEventListener("DOMContentLoaded", function(event){
+  const menuToggle = document.getElementById("menu-toggle");
+  const navbar = document.getElementById('navbar');
+  const burger = document.querySelectorAll('.nav-links a');
+  const go_up = document.getElementById('go_up_button');
 
-menuToggle.addEventListener('click', () => {
-    if (navbar.style.display == 'none'){
-      navbar.style.display == 'block';
-    } else {
-      navbar.style.display = 'none';
+  // console.log(burger);
+  menuToggle.addEventListener("click", () => {
+    // console.log("we good till here");
+      navbar.classList.toggle("active");
+  });
+
+  burger.forEach(link => {
+    link.addEventListener('click', event => {
+        navbar.classList.toggle("active");
+    })
+  })
+
+  window.addEventListener("scroll" , () => {
+    if (window.scrollY > (screen.availHeight)) {
+      go_up.style.display = 'block';
+    }else {
+      go_up.style.display = 'none';
     }
+  })
 });
